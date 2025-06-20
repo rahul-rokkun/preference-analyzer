e4413b88929f4aca24909e1b916425de95e9a339
rahul-rokkun <rahul@rokkun.io>
2025-06-20 17:05:01 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+from google import genai
+
+# Initialize Gemini API
+client = genai.Client(api_key="AIzaSyBAh1prTlOpOwZnxG3djrxtu81jqDvMbis")
+app = FastAPI()
+
+# Request model
+
+
+class ProductAnalysisRequest(BaseModel):
+    recommended_products: List[str]
+    clicked_products: List[str]
+    user_inputs: List[str]
+
+
+class ResponseModel(BaseModel):
+    category: str
+    analysis: str
+
+
+# Endpoint
+@app.post("/analyze")
+async def analyze_products(data: ProductAnalysisRequest):
+    prompt = f"""
+You are a product analysis AI assistant.
+
+Input:
+- Recommended Products: {data.recommended_products}
+- Clicked Products: {data.clicked_products}
+- User Queries: {data.user_inputs}
+
+Tasks:
+1. From the recommended and clicked product names, identify the most common product category(dress, pants, tops/blouses ,jackets/blazers).
+2. From the user inputs, identify:
+   - Common themes or patterns
+   - Any weird, outlier, or unexpected queries
+   - The general vibe (e.g., are people excited, confused, nostalgic, etc.)
+
+Provide your answer as a structured Json output.
+    category: <category>
+    analysis: <analysis>
+
+Keep the response concise with only the most relevant information within 15 - 20 words.
+    """
+
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt,
+            config={
+                "response_mime_type": "application/json",
+                "response_schema": ResponseModel,
+            },
+        )
+        return {
+            "category": response.parsed.category,
+            "analysis": response.parsed.analysis
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
